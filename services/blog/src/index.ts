c7dd83a09d19f298f8082a048c12478e7fcd620a
Barratt Isherwood <b4rr4tt@gmail.com>
2026-04-04 16:22:20 +0200

    fix: allow Authorization header in CORS

--- services/blog/src/index.ts
+++ services/blog/src/index.ts
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,x-api-key');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,x-api-key,Authorization');
   if (req.method === 'OPTIONS') { res.sendStatus(204); return; }
   next();
 });
